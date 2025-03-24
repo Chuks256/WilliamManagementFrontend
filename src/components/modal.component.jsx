cfb1315ede65c82783709c49d1e442a66170258c
chuks <alexanderchuks310@gmail.com>
2025-03-24 18:02:03 +0100

    still working

--- src/components/modal.component.jsx
+++ src/components/modal.component.jsx
@@ -9,14 +9,12 @@ left:0;
 background:rgba(0, 0, 0, 0.43);
 width:100%;
 height:100%;
-z-index:1;
 display:none;
 backdrop-filter:blur(3px);
 justify-content:center;
 `;
 
 const ModalBoxContainer = styled.div`
-padding:50px;
 background:ivory;
 height:300px;
 width:340px;
@@ -28,10 +26,14 @@ align-items:center;
 flex-direction:column;
 gap:1em;
 transition:linear,500ms;
+@media(max-width:768px){
+display:none;    
+}
 `;
 
 const ModalTxtParentContainer = styled.div`
 text-align:center;
+
 `;
 
 const ModalExitButton = styled.button`
@@ -44,6 +46,66 @@ background:var( --theme-primary-theme-color);
 font-weight:bold;
 `;
 
+const BottomSheetParentContainer = styled.div`
+position:absolute;
+background:ivory;
+height:550px;
+width:100%;
+bottom:0;
+border-radius:20px 20px 0px 0px;
+display:none;
+
+@media(max-width:768px){    
+display:block;
+}
+`;
+
+const BottomParentContainer = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+margin-top:35px;
+`;
+
+const BottomSheetLogoParentContainer = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+gap:1em;
+`;
+
+const BottomSheetLogoContainer = styled.div`
+font-size:30px;
+background:var(--theme-primary-theme-color);
+padding:15px;
+border-radius:100px;
+text-align:center;
+justify-content:center;
+align-items:center;
+`;
+const BottomSheetTitle = styled.h3`
+font-size:20px;
+`;
+const ParentContainer = styled.div`
+display:flex;
+flex-direction:column;
+gap:0.6em;
+`;
+
+const BottomSheetContentContainer = styled.div``;
+
+const CloseBottomSheetBtn=styled.button`
+padding:17px;
+font-size:15px;
+font-weight:bold;
+border-radius:10px;
+width:320px;
+margin-top:260px;
+color:var(--theme-sub-color);
+background:var(--theme-primary-theme-color);
+`;
 
 let ModalComponent=(props)=>{
     let HandleCloseBtn=()=>{
@@ -52,15 +114,31 @@ let ModalComponent=(props)=>{
     }
 return(
     <>
-    {
+    
             <ModalParentContainer id="modal_box">
             <ModalBoxContainer>
                 <ModalTxtParentContainer>{props.msg}</ModalTxtParentContainer>
                 <ModalExitButton onClick={HandleCloseBtn}>Close</ModalExitButton>
             </ModalBoxContainer>
+
+            {/* BOTTOM SHEET CONTAINER  */}
+            <BottomSheetParentContainer>
+                <BottomParentContainer>
+                
+                <ParentContainer>
+                <BottomSheetLogoParentContainer>
+                        <BottomSheetLogoContainer>🚩</BottomSheetLogoContainer>
+                        <BottomSheetTitle>Incorrect Credential</BottomSheetTitle>
+                    </BottomSheetLogoParentContainer>
+                    <BottomSheetContentContainer>The data you provided is incorrect</BottomSheetContentContainer>
+                </ParentContainer>
+                <CloseBottomSheetBtn onClick={HandleCloseBtn}>Close</CloseBottomSheetBtn>
+                </BottomParentContainer>
+            </BottomSheetParentContainer>
+
         </ModalParentContainer>
-       
-    }
+
+    
     </>
 )
 }
